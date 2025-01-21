edb155ba53d5de9da19500965151b513f04995c9
rajan <egoistrajan33@gmail.com>
2025-01-21 23:52:10 +0530

    second commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,14 +11,16 @@ const About = () => {
         </div>
         <div className='mt-5 flex justify-between gap-4'>
           <div className='text-gray-500'>
-          <p>This is a story which is about my past life that i have been experienced. When i was child my life was so good and i was not a 
-            smart boy, i was just an innocent guy who used to play game and also good at all games that he used to played So, life was running smoothly with no aim, dream
-             and future carrier then my life gets turn when i entered in 9th standard because things totally depend on environmen
-             t surrounded by you
-             So before 9th standard i was surrounded by those friend Whose had also not interested in studies having no dreams they wer
-             e mostly like me but after 9th class my circle got change
-             d i got friend like those had some dreams to become something and hungry for scoring marks in exam
-              So i also got influenced for study and</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur 
+            veniam sunt ab maiores, voluptate magni id aspernatur dolores perferendis
+             architecto laborum blanditiis? Cum ducimus cupiditate adipisci illum veniam similique
+              distinctio.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur 
+              tempore harum, atque qui at non dolore quis dolores fugiat, accusamus quod 
+              repudiandae veritatis quaerat cumque quas doloribus magni animi culpa?
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur 
+              doloribus est saepe quae officia. Consequatur, sed. Sit laboriosam minus,
+               eius, maxime quam, consequatur dolores accusamus ipsam quasi autem magnam consectetur!</p>
               <p className='mt-4'>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, cupiditate molestiae, tene
                 tur fugit animi fugiat soluta pariatur nesciunt quia ducimus velit iusto officia minus aliquam eaque 
